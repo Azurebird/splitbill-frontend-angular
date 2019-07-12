70dd1024941d96e0dd2bfc58b89f28d73ea1d691
Azurebird <white.camilo@gmail.com>
2019-07-11 19:45:18 -0500

    login successfully moved to its own module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
+import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent
   ],
   imports: [
+    AuthModule,
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
   ],
+  // The set of injectable objects that are available in the injector of this module.
   providers: [],
   bootstrap: [AppComponent]
 })
