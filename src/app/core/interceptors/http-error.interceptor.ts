2ab4928ee342d8fcf3d9488d02ca47ff6c7a4fad
Azurebird <white.camilo@gmail.com>
2019-07-16 18:06:47 -0500

    Http interceptor error handling implemented

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -1,10 +1,27 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
+/**
+ * Http interceptor to handle error response from server and translate them
+ */
 @Injectable()
-export class HttpErrorInterceptor implements HttpInterceptor{
+export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    return next.handle(req)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+          return throwError(errorMessage);
+        })
+      )
   }
 }
