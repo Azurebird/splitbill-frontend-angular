fcded0f321f59375f0213ec8a75d782d91262e81
Azurebird <white.camilo@gmail.com>
2019-08-04 21:44:35 -0500

    Sign in redirecting to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'auth', loadChildren: './modules/auth/auth.module#AuthModule'},
+  {path: 'home', loadChildren: './modules/home/home.module#HomeModule'},
   {path: '', redirectTo: 'auth', pathMatch: 'full'},
 ];
 
